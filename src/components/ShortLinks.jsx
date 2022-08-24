6a805637c8dababa946285a50154ed565f847734
Daria Smirnova <darya.smirn1@yandex.ru>
2022-08-24 20:35:19 +0300

    add saving links to local storage

--- src/components/ShortLinks.jsx
+++ src/components/ShortLinks.jsx
@@ -1,9 +1,17 @@
 import { selectLinks } from "../store/slice/linkSlice";
 import { useSelector } from "react-redux";
 import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 export const ShortLinks = () => {
   const links = useSelector(selectLinks);
+  const [copiedLink, setCopiedLink] = useState(null);
+
+  const copyToClipboard = (link) => {
+    navigator.clipboard.writeText(link).then(() => {
+      setCopiedLink(link);
+    });
+  };
 
   return (
     <section>
@@ -11,12 +19,21 @@ export const ShortLinks = () => {
         <AnimatePresence key={l.code}>
           <motion.div
             initial={{ opacity: 0, height: 0 }}
-            animate={{ opacity: 1, heoght: "auto" }}
-            style={{ display: "flex", justifyContent: "space-between" }}
+            animate={{ opacity: 1, height: "auto" }}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
           >
             <span>{l.original_link}</span>
             <span>{l.full_short_link2}</span>
-            <button>Copy</button>
+            <button
+              onClick={() => {
+                copyToClipboard(l.full_short_link2);
+              }}
+            >
+              {copiedLink === l.full_short_link2 ? "Copied!" : "Copy"}
+            </button>
           </motion.div>
         </AnimatePresence>
       ))}
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import linkReducer from "./slice/linkSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  links: linkReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    links: linkReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
