b0749ab79465f1e780cd17ca0889584165865ece
Daria Smirnova <darya.smirn1@yandex.ru>
2022-08-24 19:21:14 +0300

    add form validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,31 @@
+import { useForm } from "react-hook-form";
+
 export const Form = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    //reset,
+  } = useForm({ mode: "onSubmit" });
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <form>
-      <input type="url" placeholder="Введите ссылку" />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="url"
+        placeholder="Введите ссылку"
+        {...register("Url", {
+          required: "Пожалуйста, введите ссылку",
+          pattern: {
+            value:
+              /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
+            message: "Пожалуйста, введите валидный URL",
+          },
+        })}
+      />
       <button type="submit">Go!</button>
+      {errors.Url && <div>{errors.Url.message}</div>}
     </form>
   );
 };
